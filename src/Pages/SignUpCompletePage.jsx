0ea4df3cc2ae80e8e3192c7ba2381e98f0e2af3b
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-07-31 20:53:50 +0900

    user addinfo css coimplete
    css는 대충 끝났습니다

--- src/Pages/SignUpCompletePage.jsx
+++ src/Pages/SignUpCompletePage.jsx
@@ -4,7 +4,7 @@ import Header from '../components/SignUpComplete/Header';
 
 const SignUpCompletePage = () => {
     return (
-        <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full bg-white max-w-[480px] max-h-[500px]">
+        <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full bg-white max-w-[480px]">
             <Header/>
             <CompleteButton/>
         </main>
--- src/components/SignUpComplete/Header.jsx
+++ src/components/SignUpComplete/Header.jsx
@@ -2,8 +2,9 @@
 
 const Header = () => {
     return (
-        <section className='self-stretch pb-[60px] flex flex-col justify-center items-center'>
-            <h1 className='text-3xl font-semibold tracking-tighter leading-10 text-gray-900'>
+        <section className='self-stretch pb-[60px] flex flex-col justify-center items-center mt-44'>
+            <img src="/assets/signupcomplete.png"/>
+            <h1 className='text-3xl font-semibold tracking-tighter leading-10 text-gray-900 mt-8'>
                 회원가입 완료!
             </h1>
             <p className='mt-3.5 text-[15px] font-medium tracking-tight text-neutral-500'>
