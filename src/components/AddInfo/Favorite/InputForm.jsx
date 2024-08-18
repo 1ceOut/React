d914e41d452930563259852181f80aefebfce867
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-18 13:44:53 +0900

    중간 저장
    중간 저장

--- src/components/AddInfo/Favorite/InputForm.jsx
+++ src/components/AddInfo/Favorite/InputForm.jsx
@@ -9,14 +9,14 @@ const InputForm = () => {
   };
 
   return (
-    <div className='self-stretch w-[480px]'>
-      <div className="self-stretch border rounded-xl w-[342px] flex items-center my-4">
+    <div className='self-stretch w-[342px]'>
+      <div className="self-stretch border rounded-[12px] w-[342px] flex justify-center items-center my-4">
         <input
           id="food"
           name="food"
           type="text"
           placeholder="좋아하는 음식을 적어주세요."
-          className="block outline-none px-5 w-full h-14 text-gray-900 placeholder:text-[#A8A8A8]"
+          className="block outline-none w-[302px] h-14 text-gray-900 placeholder:text-[#A8A8A8]"
           value={inputValue}
           onChange={handleInputChange}
         />
