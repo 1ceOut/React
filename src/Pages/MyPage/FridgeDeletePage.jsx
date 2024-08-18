b4f757ca4f75615e8773354defb0cd773d288d04
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-18 15:33:13 +0900

    save
    save

--- src/Pages/MyPage/FridgeDeletePage.jsx
+++ src/Pages/MyPage/FridgeDeletePage.jsx
@@ -6,7 +6,7 @@ import FridgeSelect from '../../components/Mypage/FridgeDelete/FridgeSelect';
 const FridgeDeletePage = () => {
     return (
         // 공통 기본 틀
-        <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full max-w-[390px] h-screen" >
+        <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full max-w-[390px] h-screen">
             {/* 최상단 메뉴 */}
             <MenuNavigate option={"내 정보"} alertPath="/addinfo/habit"/>
             <Header/>
--- src/Pages/MyPage/UserDeletePage.jsx
+++ src/Pages/MyPage/UserDeletePage.jsx
@@ -1,10 +1,13 @@
+import MenuNavigate from "../../components/Common/MenuNavigate";
+import Header from "../../components/Mypage/UserDelete/Header";
 
 
 const UserDeletePage = () => {
     return (
-        <div>
-            
-        </div>
+        <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full max-w-[390px] h-screen">
+            <MenuNavigate option="구성원 삭제"/>
+            <Header/>
+        </main>
     );
 };
 
