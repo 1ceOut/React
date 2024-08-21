d3a93626f764041f5e494a93b3f98d27d56ad35c
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-21 16:13:50 +0900

    css
    css

---
+++ src/components/Mypage/Profile/LogoutButton.jsx
@@ -0,0 +1,11 @@
+
+
+const LogoutButton = () => {
+    return (
+        <div className="w-[342px] h-[56px] bottom-[54px] absolute bg-red-600 rounded-xl text-white flex justify-center items-center">
+            로그아웃
+        </div>
+    );
+};
+
+export default LogoutButton;
