f46494b04413d22c723746e36aa830232e24997e
jangwoolee <8282qwe@gmail.com>
2024-08-22 19:10:43 +0900

    logout button hover

--- src/components/Mypage/Profile/LogoutButton.jsx
+++ src/components/Mypage/Profile/LogoutButton.jsx
@@ -15,7 +15,7 @@ const LogoutButton = () => {
         });
     }
     return (
-        <div className="w-[342px] h-[56px] bottom-[54px] absolute bg-red-600 rounded-xl text-white flex justify-center items-center" onClick={LogoutAction}>
+        <div className="w-[342px] h-[56px] bottom-[54px] absolute bg-red-600 rounded-xl text-white flex justify-center items-center hover:red-800" onClick={LogoutAction}>
             로그아웃
         </div>
     );
