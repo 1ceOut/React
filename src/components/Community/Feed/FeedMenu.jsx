d3053b97db1c7de0b2cad20bcff28e3c91a9de9c
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-23 11:09:37 +0900

    save
    save

--- src/components/Community/Feed/FeedMenu.jsx
+++ src/components/Community/Feed/FeedMenu.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 
 
 const FeedMenu = () => {
+
+    const [isHidden, setIsHidden] = useState(false);
+
+    const closeHidden = () => {
+        setIsHidden(false);
+    }
+
+    const showHidden = () => {
+        setIsHidden(true);
+    }
+
     return (
         <div className="self-stretch">
             <div className="flex flex-col font-medium text-[#767676]">
@@ -16,7 +28,7 @@ const FeedMenu = () => {
                         <img src="/assets/heart.png" alt="하트" className="mr-[4px]"/>
                         좋아요
                     </div>
-                    <div className="flex justify-center items-center">
+                    <div className="flex justify-center items-center cursor-pointer" onClick={showHidden}>
                         <img src="/assets/comment.png" alt="댓글" className="mr-[4px]"/>
                         댓글쓰기
                     </div>
@@ -26,6 +38,20 @@ const FeedMenu = () => {
                     </div>
                 </div>
             </div>
+            {isHidden && (
+                <div>
+                    <div className="border-[2px] w-full min-h-16 h-auto mt-2 rounded-3xl flex justify-center items-center">
+                        <input
+                            id="food"
+                            name="food"
+                            type="text"
+                            placeholder="댓글 추가..."
+                            className="block outline-none w-[302px] h-14 pl-3 rounded-3xl text-gray-900 placeholder:text-[#A8A8A8]"
+                        />
+                        <img src="/assets/right.png" alt="입력 이모티콘" className="w-5 h-5" onClick={closeHidden}/>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
