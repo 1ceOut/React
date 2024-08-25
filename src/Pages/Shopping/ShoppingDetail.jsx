f1bde5122cd86faca5b0eed1694ef7b087c4a1b9
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-25 14:05:19 +0900

    save
    save

--- src/Pages/Shopping/ShoppingDetail.jsx
+++ src/Pages/Shopping/ShoppingDetail.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import BarNavigate from "../../components/Common/BarNavigate";
 import MenuNavigate from "../../components/Common/MenuNavigate";
 import DetailMainContent from "../../components/Shopping/DetailMainContent";
 
 const ShoppingDetail=()=>{
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     return(
         <main className="flex flex-col items-center px-6 pt-5 pb-2 mx-auto w-full max-w-[390px] h-screen">
             <MenuNavigate option={"식재료 추천"} alertPath="/addinfo/habit" />
-            <DetailMainContent/>
-            <BarNavigate 
-                shoppingsrc="/assets/shoppingselect.png"
-                homesrc="/assets/home.png"
-                searchsrc="/assets/search.png"
-            />
+            <DetailMainContent setIsModalVisible={setIsModalVisible} />
+            <div className={`w-full ${isModalVisible ? 'opacity-0' : ''}`}>
+                <BarNavigate 
+                    shoppingsrc="/assets/shoppingselect.png"
+                    homesrc="/assets/home.png"
+                    searchsrc="/assets/search.png"
+                />
+            </div>
         </main>
     )
 }
