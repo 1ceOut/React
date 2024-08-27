dd96a4501fd9684e80c203a38db0512fa5000806
PASTELBOX <147224562+PASTELBOX@users.noreply.github.com>
2024-08-27 15:35:44 +0900

    save
    save

--- src/components/Mypage/UserInvite/InviteButton.jsx
+++ src/components/Mypage/UserInvite/InviteButton.jsx
@@ -1,17 +1,15 @@
-
-
 const InviteButton = () => {
-    return (
-        <div className="w-[342px] h-[56px] bottom-[54px] absolute rounded-xl border-[1px] flex justify-center items-center font-medium text-[16px]">
-            <input
-            id="userinvite"
-            name="userinvite"
-            type="text"
-            placeholder="초대코드를 입력해주세요"
-            className="block outline-none w-[302px] h-14 text-gray-900 placeholder:text-[#A8A8A8]"
-          />
-        </div>
-    );
+  return (
+    <div className="w-[342px] h-[56px] bottom-[54px] absolute rounded-xl border-[1px] flex justify-center items-center font-medium text-[16px]">
+      <input
+        id="userinvite"
+        name="userinvite"
+        type="text"
+        placeholder="초대코드를 입력해주세요"
+        className="block outline-none w-[302px] h-14 border-y-[1px] text-gray-900 placeholder:text-[#A8A8A8]"
+      />
+    </div>
+  );
 };
 
 export default InviteButton;
