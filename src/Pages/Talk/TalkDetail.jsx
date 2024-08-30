82d7fb851b1a4af64a64dde1f367095b02707bec
LeeGaHyun12 <bearsi012@naver.com>
2024-08-30 18:01:25 +0900

    TalkDetail.

--- src/Pages/Talk/TalkDetail.jsx
+++ src/Pages/Talk/TalkDetail.jsx
@@ -108,13 +108,13 @@ const TalkDetail = () => {
             <div className="flex flex-col w-[390px] h-full bg-gray-50">
                 {/* 공지사항 */}
                 <div className="bg-gray-100 p-3 text-center text-sm text-gray-600 flex items-center justify-between">
-                    <img src="/assets/alert_icon.png" alt="확성기" className="w-5 h-5" />
+                    <img src="/assets/alert_icon.png" alt="확성기" className="w-5 h-5"/>
                     <span className="w-[250px] overflow-hidden h-4">{announcement}</span> {/* 공지사항 내용 */}
                     <button
                         className="text-gray-500 hover:text-gray-700"
                         onClick={toggleAnnouncementVisibility}
                     >
-                        {isAnnouncementVisible ? <FaChevronUp /> : <FaChevronDown />}
+                        {isAnnouncementVisible ? <FaChevronUp/> : <FaChevronDown/>}
                     </button>
                 </div>
 
