7eee3249d1653699d9583e569cd79eff73bdb4e2
JUYONG0133 <qwer99633@naver.com>
2024-09-05 15:55:45 +0900

    refri_update_0905_15:55

--- src/components/Mypage/UserDelete/FridgeUser.jsx
+++ src/components/Mypage/UserDelete/FridgeUser.jsx
@@ -1,13 +1,12 @@
 // FridgeUser.jsx
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import useUserStore from "../../../store/useUserStore.js";
-import {useNavigate} from "react-router-dom";
-import {masterUserList} from "../../../query/RefriQuery.jsx";
+import { useNavigate } from "react-router-dom";
+import { masterUserList } from "../../../query/RefriQuery.jsx";
 import UserSelect from "./UserSelect.jsx";
 import UserDeleteButton from "./UserDeleteButton";
 
 const FridgeUser = () => {
-
     const [userData, setUserData] = useState([]);
     const [checkedUsers, setCheckedUsers] = useState({}); // 냉장고별 체크된 유저를 관리
     const { userId, isLogin, LoginSuccessStatus } = useUserStore();
@@ -50,33 +49,44 @@ const FridgeUser = () => {
     // 모든 냉장고에서 체크된 유저들을 합쳐서 확인
     const allCheckedUsers = Object.values(checkedUsers).flat();
     const isAnyChecked = allCheckedUsers.length > 0;
-    const checkedUserIds = allCheckedUsers.map(user => user.id);
-    const checkedUserNames = allCheckedUsers.map(user => user.name);
+    const checkedUserIds = allCheckedUsers.map((user) => user.id);
+    const checkedUserNames = allCheckedUsers.map((user) => user.name);
 
     return (
         <div className="self-stretch flex flex-col w-[342px] mb-5">
-            <div className="text-base font-semibold flex flex-col overflow-x-hidden h-[410px]">
-                {userData.map((refri, index) => (
-                    <div key={index} className="items-center space-x-2 mb-2">
-                        <div className="bg-blue-100 text-xl font-bold text-blue-800 p-3 rounded-lg shadow-md mb-3 text-center">
-                            {refri.refrigeratorName} 냉장고
-                        </div>
-                        <div>
-                            <UserSelect
-                                userId={userId}
-                                refriId={refri.refrigerator_id}
-                                onUserSelectChange={handleUserSelectChange} // 변경된 체크 상태를 전달
-                            />
-                        </div>
+            {userData.length === 0 ? (
+                <div className="space-y-8 mt-16">
+                    <div className="flex justify-center items-center">
+                        <img src={"/assets/confirm.png"} alt="confirm"/>
                     </div>
-                ))}
-            </div>
-            <UserDeleteButton
-                isEnabled={isAnyChecked}
-                checkedUserIds={checkedUserIds}
-                checkedUserNames={checkedUserNames}
-                refriId={userData.length > 0 ? userData[0].refrigerator_id : ""}
-            />
+                    <center><p>등록된 냉장고가 없습니다</p></center>
+                </div>
+            ) : (
+                <>
+                    <div className="text-base font-semibold flex flex-col overflow-x-hidden h-[410px]">
+                        {userData.map((refri, index) => (
+                            <div key={index} className="items-center space-x-2 mb-2">
+                            <div className="bg-blue-100 text-xl font-bold text-blue-800 p-3 rounded-lg shadow-md mb-3 text-center">
+                                    {refri.refrigeratorName} 냉장고
+                                </div>
+                                <div>
+                                    <UserSelect
+                                        userId={userId}
+                                        refriId={refri.refrigerator_id}
+                                        onUserSelectChange={handleUserSelectChange} // 변경된 체크 상태를 전달
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <UserDeleteButton
+                        isEnabled={isAnyChecked}
+                        checkedUserIds={checkedUserIds}
+                        checkedUserNames={checkedUserNames}
+                        refriId={userData.length > 0 ? userData[0].refrigerator_id : ""}
+                    />
+                </>
+            )}
         </div>
     );
 };
