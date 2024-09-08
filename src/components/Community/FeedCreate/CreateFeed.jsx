d6e81d9ed7724fc13166a933321611f754584d12
kongbh730 <80806206+kongbh730@users.noreply.github.com>
2024-09-09 00:35:40 +0900

    Update CreateFeed.jsx

--- src/components/Community/FeedCreate/CreateFeed.jsx
+++ src/components/Community/FeedCreate/CreateFeed.jsx
@@ -82,12 +82,12 @@ const CreateFeed = () => {
       try {
         await addPost(postingData);
 
-        // 알림 전송 // 포스팅 작성
-        await axios.post(`${import.meta.env.VITE_ALERT_IP}/writePosting`, null, {
-          params: {
-            sender: userId,
-          },
-        });
+        // // 알림 전송 // 포스팅 작성
+        // await axios.post(`${import.meta.env.VITE_ALERT_IP}/writePosting`, null, {
+        //   params: {
+        //     sender: userId,
+        //   },
+        // });
 
         navigate("/community/feed");
       } catch (err) {
